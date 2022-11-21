9c316042c3872b574657c11679d3280b76ed2b6c
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-21 15:15:19 -0500

    Add files via upload

--- nutrition/nutrition.py
+++ nutrition/nutrition.py
@@ -1,33 +1,32 @@
-import sys
-import os
-from PIL import Image
-from PIL import ImageOps
+Fruits = {
+    "apple": 130,
+    "avocado": 50,
+    "banana": 110,
+    "Cantaloupe": 50,
+    "grapefruit": 60,
+    "grapes": 90,
+    "honeydew melon": 50,
+    "kiwifruit": 90,
+    "lemon": 15,
+    "lime": 20,
+    "nectarine": 60,
+    "orange": 80,
+    "peach": 60,
+    "pear": 100,
+    "pineapple": 50,
+    "plums": 70,
+    "strawberries": 50,
+    "sweet cherries": 100,
+    "tangerine": 50,
+    "watermelon": 80
+}
 
-try:
-    if len(sys.argv) <= 2:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 3:
-        sys.exit("Too many command-line arguments")
+I = input("Items: ").lower()
+
+while True:
+    if I in Fruits:
+        c = Fruits.get(I)
+        print(F"Calories: {c}")
+        break
     else:
-        i = os.path.splitext(sys.argv[1])
-        o = os.path.splitext(sys.argv[2])
-        if not i[1] == ".png" and not i[1] == ".jpg" and not i[1] == ".jpeg":
-            sys.exit("Invalid input")
-        elif not o[1] == ".png" and not o[1] == ".jpg" and not o[1] == ".jpeg":
-            sys.exit("Invalid output")
-        elif o[1] != i[1]:
-            sys.exit("Input and output have different extensions")
-        elif os.path.isfile(sys.argv[1]) is False:
-            sys.exit("Input does not exist")
-        else:
-            person = Image.open(sys.argv[1])
-            shirt = Image.open("shirt.png")
-            width, height = shirt.size
-            p = ImageOps.fit(person, (width, height))
-            p.paste(shirt, (0, 0), shirt)
-            p.save(
-                sys.argv[2]
-            )
-
-except FileNotFoundError:
-    sys.exit("File does not exist")
+        break
