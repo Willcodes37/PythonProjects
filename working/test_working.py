73ec40bd886c7ad3c969e501f072417480f2e6ad
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 15:10:48 -0500

    Add files via upload

--- working/test_working.py
+++ working/test_working.py
@@ -1,81 +1,42 @@
-import re
-
-am = {
-    "12": "00",
-    "1": "01",
-    "2": "02",
-    "3": "03",
-    "4": "04",
-    "5": "05",
-    "6": "06",
-    "7": "07",
-    "8": "08",
-    "9": "09",
-    "10": "10",
-    "11": "11"
-}
-
-pm = {
-    "12": "12",
-    "1": "13",
-    "2": "14",
-    "3": "15",
-    "4": "16",
-    "5": "17",
-    "6": "18",
-    "7": "19",
-    "8": "20",
-    "9": "21",
-    "10": "22",
-    "11": "23"
-}
+from working import convert
+import pytest
 
 
 def main():
-    print(convert(input("Hours: ")))
+    test_case_words()
+    test_case_valid()
+    test_case_symbols()
+    test_case_without_to()
+    test_case_outofrange()
 
 
-def convert(s):
+def test_case_words():
+    with pytest.raises(ValueError):
+        convert('cat AM to chicken PM')
+        convert('Hello AM to Bye PM')
 
-    x = re.search(r"^([1-9]|1[0-2])(:[0-5]{1}[0-9]{1})\s(AM|PM)\sto\s([1-9]|1[0-2])(:[0-5]{1}[0-9]{1})\s(AM|PM)$", s)
-    x2 = re.search(r"^([1-9]|1[0-2])\s(AM|PM)\sto\s([1-9]|1[0-2])?\s(AM|PM)$", s)
 
-    if x:
-        if x.group(3) == "AM":
-            h = am.get(x.group(1))
-            m = x.group(2)
-        else:
-            h = pm.get(x.group(1))
-            m = x.group(2)
+def test_case_valid():
+    assert convert("8:50 AM to 5:30 PM") == "08:50 to 17:30"
+    assert convert("9:00 AM to 3:20 PM") == "09:00 to 15:20"
 
-        if x.group(6) == "AM":
-            h2 = am.get(x.group(4))
-            m2 = x.group(5)
-        else:
-            h2 = pm.get(x.group(4))
-            m2 = x.group(5)
 
-        return h + m + " " + "to" + " " + h2 + m2
+def test_case_symbols():
+    with pytest.raises(ValueError):
+        convert("@#$ to *%$")
+        convert("123 to ^%2")
 
-    elif x2:
-        if x2.group(2) == "AM":
-            h = am.get(x2.group(1))
-            m = "00"
-        else:
-            h = pm.get(x2.group(1))
-            m = "00"
 
-        if x2.group(4) == "AM":
-            h2 = am.get(x2.group(3))
-            m2 = "00"
-        else:
-            h2 = pm.get(x2.group(3))
-            m2 = "00"
+def test_case_without_to():
+    with pytest.raises(ValueError):
+        convert("9:50 AM 6:30 PM")
+        convert("12:50 AM 8:30 PM")
 
-        return h + ":" + m + " " + "to" + " " + h2 + ":" + m2
 
-    else:
-        raise ValueError
+def test_case_outofrange():
+    with pytest.raises(ValueError):
+        convert("9:70 AM to 6:30 PM")
+        convert("12:50 AM to 8:80 PM")
 
 
 if __name__ == "__main__":
