697fb8c87266fec0b492f4b1fe0218b0e470e87f
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 16:00:35 -0500

    Add files via upload

--- response/response.py
+++ response/response.py
@@ -1,62 +1,18 @@
-from datetime import date
-import inflect
-import re
-import sys
+from validator_collection import checkers
 
 
 def main():
-    date_input = input("Date of birth: ")
-    print(convert(date_input))
+    print(validate(input("Text: ")))
 
 
-def convert(a):
-    p = inflect.engine()
-    c = re.search("^([1-2]{1}[0-9]{3})-(0[1-9]|1[0-2])-(0[1-9]|1[0-9]|2[0-9]|3[0-1])$", a)
+def validate(s):
 
-    if c:
-        x, y, z = a.split("-")
-        if y[0] == "0" and z[0] == "0":
-            y = y.replace("0", "")
-            z = z.replace("0", "")
-            if date(int(x), int(y), int(z)):
-                days = date.today() - date(int(x), int(y), int(z))
-                days = str(days)
-                days_index = days.split(" ")
-                minutes = round(int(days_index[0]) * 24 * 60)
-                return p.number_to_words(minutes).replace("and ", "").capitalize() + " minutes"
-            else:
-                sys.exit("Invalid date")
-        elif y[0] == "0" and not z[0] == "0":
-            y = y.replace("0", "")
-            if date(int(x), int(y), int(z)):
-                days = date.today() - date(int(x), int(y), int(z))
-                days = str(days)
-                days_index = days.split(" ")
-                minutes = round(int(days_index[0]) * 24 * 60)
-                return p.number_to_words(minutes).replace("and ", "").capitalize() + " minutes"
-            else:
-                sys.exit("Invalid date")
-        elif not y[0] == "0" and z[0] == "0":
-            z = z.replace("0", "")
-            if date(int(x), int(y), int(z)):
-                days = date.today() - date(int(x), int(y), int(z))
-                days = str(days)
-                days_index = days.split(" ")
-                minutes = round(int(days_index[0]) * 24 * 60)
-                return p.number_to_words(minutes).replace("and ", "").capitalize() + " minutes"
-            else:
-                sys.exit("Invalid date")
-        else:
-            if date(int(x), int(y), int(z)):
-                days = date.today() - date(int(x), int(y), int(z))
-                days = str(days)
-                days_index = days.split(" ")
-                minutes = round(int(days_index[0]) * 24 * 60)
-                return p.number_to_words(minutes).replace("and ", "").capitalize() + " minutes"
-            else:
-                sys.exit("Invalid date")
+    valid_email = checkers.is_email(s)
+
+    if valid_email is True:
+        return "Valid"
     else:
-        sys.exit("Invalid date")
+        return "Invalid"
 
 
 if __name__ == "__main__":
