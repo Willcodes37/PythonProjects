504532507385f1044725359389aa641e593eb24a
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 15:05:00 -0500

    Add files via upload

--- watch/watch.py
+++ watch/watch.py
@@ -1,42 +1,21 @@
-from working import convert
-import pytest
+import re
 
 
 def main():
-    test_case_words()
-    test_case_valid()
-    test_case_symbols()
-    test_case_without_to()
-    test_case_outofrange()
+    print(parse(input("HTML: ")))
 
 
-def test_case_words():
-    with pytest.raises(ValueError):
-        convert('cat AM to chicken PM')
-        convert('Hello AM to Bye PM')
+def parse(s):
 
+    x = re.search(r"(https|http)://(www\.)?(youtube\.com)/embed/(\w+)", s)
 
-def test_case_valid():
-    assert convert("8:50 AM to 5:30 PM") == "08:50 to 17:30"
-    assert convert("9:00 AM to 3:20 PM") == "09:00 to 15:20"
-
-
-def test_case_symbols():
-    with pytest.raises(ValueError):
-        convert("@#$ to *%$")
-        convert("123 to ^%2")
-
-
-def test_case_without_to():
-    with pytest.raises(ValueError):
-        convert("9:50 AM 6:30 PM")
-        convert("12:50 AM 8:30 PM")
-
-
-def test_case_outofrange():
-    with pytest.raises(ValueError):
-        convert("9:70 AM to 6:30 PM")
-        convert("12:50 AM to 8:80 PM")
+    if x:
+        if not s.startswith("<iframe") and not s.endswith("></iframe>"):
+            return None
+        else:
+            return f"https://youtu.be/{x.group(4)}"
+    else:
+        return None
 
 
 if __name__ == "__main__":
