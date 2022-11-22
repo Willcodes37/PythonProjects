4567f0216b3b448b71b9d91e1165a87929734edb
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 13:26:44 -0500

    Add files via upload

--- test_bank/bank.py
+++ test_bank/bank.py
@@ -1,26 +1,19 @@
-from bank import value
-
-
 def main():
-    test_case_hello()
-    test_case_h()
-    test_case_other()
+    x = input("Greeting: ").strip()
+    greet = value(x)
+    print(greet)
 
 
-def test_case_hello():
-    assert value('Hello, Michael ') == 0
-    assert value('Hello, how are you?') == 0
-
-
-def test_case_h():
-    assert value("Hi, Adam") == 20
-    assert value("Howdy") == 20
-
-
-def test_case_other():
-    assert value("What's up?") == 100
-    assert value("Good morning") == 100
+def value(greeting):
+    if greeting.startswith("Hello"):
+        return 0
+    elif greeting.startswith("H"):
+        return 20
+    else:
+        return 100
 
 
 if __name__ == "__main__":
     main()
+
+
