e94e7e6e3be61525eb371ea7c277e96faa213075
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 13:33:44 -0500

    Add files via upload

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,27 +1,36 @@
-while True:
-    try:
+def main():
+    fraction = input("fraction: ")
+    percentage = convert(fraction)
+    fuel_reading = gauge(percentage)
+    print(fuel_reading)
 
-        Fraction = input("Fraction: ")
-        x, z = Fraction.split("/")
-        xx = int(x)
-        zz = int(z)
-        total = round((xx / zz) * 100)
-        if xx > zz:
-            pass
-        elif total >= 99:
-            print("F")
-            break
-        elif total <= 1:
-            print("E")
-            break
-        else:
-            print(f"{total}%")
-            break
 
-    except ValueError:
+def convert(fraction):
+    x, y = fraction.split("/")
+    x = int(x)
+    y = int(y)
+    z = x / y
+    if 0 <= z <= 1:
+        percent = round(z * 100)
+        return percent
+    elif x > y:
+        raise ValueError
+    elif x < 0 or y < 0:
+        raise ValueError
+    elif y == 0:
+        raise ZeroDivisionError
+    else:
         pass
 
-    except ZeroDivisionError:
-        pass
 
-    
+def gauge(percentage):
+    if percentage >= 99:
+        return "F"
+    elif percentage <= 1:
+        return "E"
+    else:
+        return f"{percentage}%"
+
+
+if __name__ == "__main__":
+    main()
