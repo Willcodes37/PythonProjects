39755bb426d8b99838b38207757bdf3b2ac44b02
William Ross <108364047+Willcodes37@users.noreply.github.com>
2022-11-22 13:36:37 -0500

    Add files via upload

--- lines/lines.py
+++ lines/lines.py
@@ -1,22 +1,24 @@
-def main():
-    c = convert(input("What time is it? "))
-    if 7 <= c <= 8:
-        print("breakfast time")
-    elif 12 <= c <= 13:
-        print("lunch time")
-    elif 18 <= c <= 19:
-        print("dinner time")
+import sys
+
+try:
+    i = 0
+    if len(sys.argv) <= 1:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    elif not sys.argv[1].endswith(".py"):
+        sys.exit("Not a Python file")
     else:
-        pass
+        with open(sys.argv[1]) as file:
+            f = file.readlines()
+            for line in f:
+                if line.lstrip().startswith("#") or line.isspace():
+                    pass
+                else:
+                    i += 1
+
+    print(i)
 
 
-def convert(time):
-    hours, minutes = time.split(":")
-    h = int(hours)
-    m = int(minutes)
-    mm = m / 60
-    return h + mm
-
-
-if __name__ == "__main__":
-    main()
+except FileNotFoundError:
+    sys.exit("File does not exist")
